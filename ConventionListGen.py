77eadcf110a0e886a0a9375dce8acee2e60c0687
Alexander Ho <39490762+hoalexander44@users.noreply.github.com>
2021-08-26 18:38:29 -0400

    Successfully scraped the convention website

--- ConventionListGen.py
+++ ConventionListGen.py
@@ -8,4 +8,17 @@ import time
 PATH = ".\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://animecons.com/events/schedule.php?year=2021")
+driver.get("https://animecons.com/events/schedule.php?loc=us&year=2021")
+con_list_table_element = driver.find_element_by_id("ConListTable")
+
+# odd and even doesn't matter its just how the website organized it
+conventions = con_list_table_element.find_elements_by_class_name("even")
+oddConventions = con_list_table_element.find_elements_by_class_name("odd")
+
+conventions.extend(oddConventions)
+
+for convention in conventions:
+	print(convention.text)
+
+driver.quit()
+
