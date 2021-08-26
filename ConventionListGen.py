e15c2c32ccd955fa1deb27c8bf4b6029c4565bad
Alexander Ho <39490762+hoalexander44@users.noreply.github.com>
2021-08-26 18:23:47 -0400

    added chrome driver and imports

--- ConventionListGen.py
+++ ConventionListGen.py
@@ -1,2 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+PATH = ".\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://animecons.com/events/schedule.php?year=2021")
